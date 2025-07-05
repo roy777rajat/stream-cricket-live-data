25d1564465a6d755b312d8307231953a8fc2c412
Rajat Roy <roy777rajat@gmail.com>
2025-07-05 16:10:02 +0100

    baseline-03072025_right

--- pyspark-batch.bat
+++ pyspark-batch.bat
@@ -1,15 +1,8 @@
 @echo off
-REM Activate virtual environment
-call E:\01. Python Basic\cricket_stream\.venv\Scripts\activate.bat
+echo Running papermill...
 
-REM Set Spark environment (optional if already in PATH)
-set SPARK_HOME=C:\spark\spark-3.5.5-bin-hadoop3
-set PATH=%SPARK_HOME%\bin;%PATH%
-set HADOOP_HOME=C:\hadoop
+"C:\Users\ADMIN\AppData\Roaming\Python\Python310\Scripts\papermill.exe" "E:\01. Python Basic\cricket_stream\cricket_stream_test.ipynb" "E:\01. Python Basic\cricket_stream\executed\output_test_from_bat.ipynb" -k python3 >> "E:\01. Python Basic\cricket_stream\logs\papermill_run.log" 2>&1
 
-REM Create output/log folders if not exist
-if not exist "E:\01. Python Basic\cricket_stream\executed" mkdir "E:\01. Python Basic\cricket_stream\executed"
-if not exist "E:\01. Python Basic\cricket_stream\logs" mkdir "E:\01. Python Basic\cricket_stream\logs"
-
-REM Run notebook with timestamped output and log
-papermill "E:\01. Python Basic\cricket_stream\my_spark_job.ipynb" "E:\01. Python Basic\cricket_stream\executed\output_%DATE:/=%%TIME::=%.ipynb" >> "E:\01. Python Basic\cricket_stream\logs\log%DATE:/=%%TIME::=_%.txt" 2>&1
+echo Papermill finished with exit code %ERRORLEVEL%
+echo Check log file for details.
+pause
